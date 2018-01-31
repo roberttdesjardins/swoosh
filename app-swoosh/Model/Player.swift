363154ad0e0eb61bb90f9bd199d26862494ca813
Robert Desjardins <robd@Roberts-MBP.hitronhub.home>
2018-01-31 13:41:07 -0800

    Lesson9 and 10

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Robert Desjardins on 2018-01-31.
+//  Copyright © 2018 Robert Desjardins. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String!
+    
+}
